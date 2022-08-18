fc46c3baabca8aa550d49790c1ef4fb2aa2a8aff
gnobu <inyangubong17@gmail.com>
2022-08-18 08:24:13 +0100

    feat: added chat routes to the backend

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const express = require('express');
 const app = express();
 
 const { PORT } = process.env || 5000;
-// const chats = require('./data/data');
 const connectDB = require('./config/db');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
-const cloudinary = require('cloudinary').v2;
 
 
 connectDB(() => {
@@ -16,7 +14,6 @@ connectDB(() => {
 
     app.get('/', (req, res) => {
         res.send('API is running');
-        // res.send(cloudinaryConfig);
     })
 
     app.use('/api/user', userRoutes);
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
+const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup } = require('../controllers/chatControllers');
 const { protect } = require('../middleware/authMiddleware');
 
 
-// router.post('/', protect, accessChat);
-// router.get('/', protect, fetchChats);
-// router.post('/group', protect, createGroupChat);
-// router.put('/rename', protect, renameGroup);
-// router.put('/groupremove', protect, removeFromGroup);
-// router.put('/groupadd', protect, addToGroup);
+router.post('/', protect, accessChat);
+router.get('/', protect, fetchChats);
+router.post('/group', protect, createGroupChat);
+router.put('/rename', protect, renameGroup);
+router.put('/groupremove', protect, removeFromGroup);
+router.put('/groupadd', protect, addToGroup);
 
 module.exports = router;
